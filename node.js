5274f93e4d28890c1046ef06d53715350b358ab1
micha-del <113006755+micha-del@users.noreply.github.com>
2023-01-18 20:36:49 -0500

    Add files via upload
    beginning to write the code for the javascript section

--- node.js
+++ node.js
@@ -1,27 +1,6 @@
-function getWeather(longitude, latitude, title) {
-
-    const longLat = `https://www.7timer.info/bin/astro.php?lon=${longitude}&lat=${latitude}&ac=0&unit=metric&output=json&tzshift=0`;
-    const area = `AIzaSyABhd9iDjrzWO-fGETW82-Rj6LN-CljREg`
-    fetch(longLat)
-      .then(response => response.json())
-      .then(data => {
-  
-        let html = `<h3>Weather for ${title}</h3>`;
-  
-        data.dataseries.forEach((item, index) => {
-          html += (`
-          <div class="day">
-            <strong>Day ${1 + index}</strong>
-            <div>Cloud Cover: ${item.cloudcover}</div>
-            <div>Wind Direction: ${item.wind10m.direction}</div>
-            <div>Wind Speed: ${item.wind10m.speed}</div>
-            <div>Temp: ${item.temp2m}</div>
-          </div>
-        `);
-        });
-  
-        weather.innerHTML = html;
-  
-      });
+let weather = {
+  "apiKey":"c737eadcc1f96817b0fda252b6010c02",
+  fetchWeather: function() {
+    fetch(https://api.openweathermap.org/data/2.5/weather?q=Alexandria&Units=imperial&appid=c737eadcc1f96817b0fda252b6010c02)
   }
- getWeather(Math.random()*360-180)
+}
